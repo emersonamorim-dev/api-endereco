373309755779b02d26ade98b4f5e8a7a23879d68
Emerson Rockman <emerson_tecno@hotmail.com>
2022-07-12 09:30:56 -0300

    Api-Endereco

---
+++ src/main/java/com/codev/endereco/http/EnderecoJson.java
@@ -0,0 +1,20 @@
+package com.codev.endereco.http;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+public class EnderecoJson {
+    
+    private String cep;
+    private Long numero;
+    private String complemento;
+}
---
+++ src/test/java/com/codev/endereco/EnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codev.endereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
